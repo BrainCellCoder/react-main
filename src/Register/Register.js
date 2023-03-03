e906288ae6ef5b26df2b3555f8c780dfecb6398d
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-03 11:28:38 +0530

    initial commit

---
+++ src/Register/Register.js
@@ -0,0 +1,117 @@
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "./Register.css";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+export const Register = () => {
+  const schema = yup.object().shape({
+    name: yup.string().required("*name is required"),
+    email: yup
+      .string()
+      .email("*please provide a valid email")
+      .required("*email is required"),
+    password: yup
+      .string()
+      .min(4, "*password must be atleast 4 characters")
+      .max(20)
+      .required("*password is required"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  // https://react-http-9d849-default-rtdb.firebaseio.com/
+  // https://rose-doubtful-moth.cyclic.app/user/register
+  const onSubmit = async (data) => {
+    console.log(data);
+    const res = await fetch(
+      "https://rose-doubtful-moth.cyclic.app/user/register",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        }),
+        //   body: data,
+      }
+    );
+    console.log(res);
+  };
+  const onError = () => {
+    console.log("Error");
+  };
+
+  <form />;
+
+  return (
+    <>
+      <Container id="register">
+        <Row>
+          <Col className="left-reg"></Col>
+          <Col className="right-reg">
+            <h1>Register</h1>
+            <Form onSubmit={handleSubmit(onSubmit, onError)} method="POST">
+              <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  name="name"
+                  //   onChange={nameChangeHandler}
+                  {...register("name")}
+                />
+                <p className="error-message">{errors.name?.message}</p>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter email"
+                  name="email"
+                  //   onChange={emailChangeHandler}
+                  {...register("email")}
+                />
+                <p className="error-message">{errors.email?.message}</p>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  name="password"
+                  //   onChange={passwordChangeHandler}
+                  {...register("password")}
+                />
+                <p className="error-message">{errors.password?.message}</p>
+              </Form.Group>
+              {/* <Form.Group className="mb-3">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Re-enter password"
+                  {...register("confirmPassword")}
+                />
+                <p className="error-message">
+                  {errors.confirmPassword?.message}
+                </p>
+              </Form.Group> */}
+              <Button variant="primary" type="submit" className="reg-button">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
