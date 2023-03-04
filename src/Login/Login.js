eb5be042766390bd4ffda4dc6198b95616d35173
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-04 21:15:26 +0530

    login page design

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -43,7 +43,8 @@ export const Login = () => {
         }),
       }
     );
-    console.log(res);
+    const resp = await res.json();
+    console.log(resp);
   };
   const onError = () => {
     console.log("Error");
