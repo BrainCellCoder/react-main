79a08d9978061c72d8d1c1a28146421918823b73
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-06 08:13:49 +0530

    feat: card component for products

---
+++ src/components/Home/Products/Headphones/Card.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Card = (props) => {
+  return (
+    <div className="col-lg-3 col-md-6">
+      <div className="product">
+        <div
+          className="image"
+          // style={{ backgroundImage: "url('././Products/Headphones/airpodmax.png')" }}
+        ></div>
+        <div className="title">
+          <h5 className="name">{props.data.name}</h5>
+          <h5 className="price">${props.data.price}</h5>
+        </div>
+        <div className="description text-muted">{props.data.description}</div>
+        <div className="ratings">
+          <p className="stars">
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+          </p>
+          <p className="number-of-reviews text-muted">(69)</p>
+        </div>
+        <div className="add-to-cart">Add to cart</div>
+        <div className="wish">
+          <i className="fa-regular fa-heart"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Home/Products/Headphones/Headphones.js
+++ src/components/Home/Products/Headphones/Headphones.js
@@ -1,253 +1,29 @@
 import React from "react";
 import "./Headphone.css";
+import { useEffect, useState } from "react";
+import { Card } from "./Card";
 
 export const Headphones = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function headphone() {
+      const res = await fetch(
+        "https://rose-doubtful-moth.cyclic.app/products?limit=8"
+      );
+      const product = await res.json();
+      setProducts(product.products);
+    }
+    headphone();
+  }, []);
   return (
     <>
       <div className="products container">
         <h3>Headphones For You!</h3>
         <div className="row gy-4">
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('././Products/Headphones/airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row gy-4 mt-2">
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="product">
-              <div
-                className="image"
-                // style={{ backgroundImage: "url('airpodmax.png')" }}
-              ></div>
-              <div className="title">
-                <h5 className="name">AirPod Max</h5>
-                <h5 className="price">$599.00</h5>
-              </div>
-              <div className="description text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-              </div>
-              <div className="ratings">
-                <p className="stars">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                </p>
-                <p className="number-of-reviews text-muted">(69)</p>
-              </div>
-              <div className="add-to-cart">Add to cart</div>
-              <div className="wish">
-                <i className="fa-regular fa-heart"></i>
-              </div>
-            </div>
-          </div>
+          {products.map((product) => {
+            return <Card data={product} />;
+          })}
         </div>
       </div>
     </>
