7c124cd9d7944dbe73ba58c0ca6f8f33268f85be
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-12 16:58:13 +0530

    feat: jwt decode

--- src/Utils/isAuth.js
+++ src/Utils/isAuth.js
@@ -1,3 +1,5 @@
+import jwt_decode from "jwt-decode";
+
 function isAuthenticated() {
   const token = localStorage.getItem("techkart_token");
 
@@ -5,13 +7,16 @@ function isAuthenticated() {
     return false;
   }
 
+  const decoded = jwt_decode(token);
+  console.log(decoded);
   return true;
+
   // verify token
   //   try {
-  //     // const decoded = jwt.verify(
-  //     //   token,
-  //     //   "c6a4f5a07c5141f7cbbfb5cde9edccead9df5b3e6611ae1c0770458e0571bf0fe4453b35bee4651cf91461e16f288bd2"
-  //     // );
+  //     const decoded = jwt.verify(
+  //       token,
+  //       "c6a4f5a07c5141f7cbbfb5cde9edccead9df5b3e6611ae1c0770458e0571bf0fe4453b35bee4651cf91461e16f288bd2"
+  //     );
   //     console.log(decoded);
   //     return true;
   //   } catch (err) {
