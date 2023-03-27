9d81c101c71f2625a8e4d94117450844002f5048
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-27 23:36:24 +0530

    feat: filter by category

--- src/components/Home/Filters/Filter.js
+++ src/components/Home/Filters/Filter.js
@@ -1,21 +1,46 @@
 import React from "react";
 import "./Filter.css";
-export const Filter = () => {
+export const Filter = (props) => {
+  const onChange = (event) => {
+    props.cat(event.target.value);
+  };
   return (
     <>
       <div className="filters container">
         <div className="category">
-          Category Type <i className="fa-solid fa-caret-down"></i>
+          <select onChange={onChange}>
+            <option>Category Type</option>
+            <option value="">All</option>
+            <option value="laptops">Laptop</option>
+            <option value="phones">Phone</option>
+            <option value="headphones">Headphone</option>
+            <option value="accessories">Accessories</option>
+          </select>
         </div>
         <div className="price">
-          Price <i className="fa-solid fa-caret-down"></i>
+          <select>
+            <option>Price</option>
+            <option value="">All</option>
+            <option value="Laptop">High-Low</option>
+            <option value="Mobile">Low-High</option>
+          </select>
         </div>
-        <div className="reviews">
-          Reviews <i className="fa-solid fa-caret-down"></i>
-        </div>
-        <div className="color">
+        {/* <div className="reviews">
+          <select>
+            <option>Reviews</option>
+            <option value="">All</option>
+            <option value="Laptop">5 stars</option>
+            <option value="Mobile">4 stars</option>
+            <option value="Mobile">3 stars</option>
+            <option value="Mobile">2 stars</option>
+            <option value="Mobile">1 stars</option>
+            <option value="Mobile">Best-Worst</option>
+            <option value="Mobile">Worst-Best</option>
+          </select>
+        </div> */}
+        {/* <div className="color">
           Color <i className="fa-solid fa-caret-down"></i>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Hero } from "./Hero/Hero";
 import { Filter } from "./Filters/Filter";
 import { Phones } from "./Products/Phones/Phones";
 import { Laptops } from "./Products/Laptops/Laptops";
 import { Hero2 } from "./Hero2/Hero2";
+import { Headphones } from "./Products/Headphones/Headphones";
+import { Accessories } from "./Products/Accessories/Accessories";
+
 export const Home = () => {
+  const [cat, setCat] = useState("");
   return (
     <>
       <Hero />
-      <Filter />
-      <Phones />
-      <Laptops />
+      <Filter cat={setCat} />
+      {cat === "phones" && <Phones />}
+      {cat === "laptops" && <Laptops />}
+      {cat === "headphones" && <Headphones />}
+      {cat === "accessories" && <Accessories />}
+      {cat === "" && (
+        <>
+          <Phones /> <Laptops />
+        </>
+      )}
       <Hero2 />
     </>
   );
